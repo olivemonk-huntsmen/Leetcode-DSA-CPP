f9324c25711879bcaf9a0b758b964ab889c65d08
olivemonk-huntsmen <mishraankit.d@gmail.com>
2026-08-10 20:21:32 +0530

    Time: 55 ms (27.54%) | Memory: 31 MB (21.08%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
- vector<vector<int>> ans;
-        
         int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
         
-        // Sort the input array nums
-        sort(nums.begin(), nums.end());
-        
-        // Iterate through the array to find triplets
-        for (int i = 0; i < n; i++) {
-            // Skip duplicates
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            
-            // Two pointers approach
-            int j = i + 1;
-            int k = n - 1;
-            
-            while (j < k) {
+            while(j<k){
                 int sum = nums[i] + nums[j] + nums[k];
-                
-                if (sum < 0) {
+                if(sum < 0){
                     j++;
-                } else if (sum > 0) {
+                }
+                else if (sum>0){
                     k--;
-                } else {
-                    // Found a triplet that sums up to target
+                }
+                else 
+                {
                     vector<int> temp = {nums[i], nums[j], nums[k]};
                     ans.push_back(temp);
-                    
-                    // Skip duplicates
                     j++;
                     k--;
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
+                    while ( j < k && nums[j] == nums[j - 1]) j++;
+                    while ( k > j && nums[k] == nums[k + 1]) k--;
+
                 }
             }
         }
-        
         return ans;
     }
 };
